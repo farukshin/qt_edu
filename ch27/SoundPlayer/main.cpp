2d3bad75622bebc14eedff972f832e35e7a9deda
Amir Farukshin <farukshin.amir@gmail.com>
2020-10-05 20:32:00 +0300

    docs(): main файл из примерапо работе со звуком

---
+++ ch27/SoundPlayer/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include "SoundPlayer.h"
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    //SoundPlayer  soundPlayer;
+
+    //soundPlayer.resize(320, 75);
+    //soundPlayer.show();
+
+    return app.exec();
+}
