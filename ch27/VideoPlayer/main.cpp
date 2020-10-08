5aae8da129ee828a7af5c68b0bc6894ca73f3f40
Amir Farukshin <farukshin.amir@gmail.com>
2020-10-08 22:52:00 +0300

    docs(): шаблон по работе с видео

---
+++ ch27/VideoPlayer/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include "VideoPlayer.h"
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    //VideoPlayer  videoPlayer;
+
+    //videoPlayer.resize(400, 450);
+    //videoPlayer.show();
+
+    return app.exec();
+}
